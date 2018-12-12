492142ecd1c37937d8e2a406e0bf02a26295f12f
Cosmin Burlacu <cosmin.burlacu@yahoo.com>
2018-12-12 23:25:21 +0100

    Updates

---
+++ precache-manifest.673af598c6f7a99156dabaa155a63cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1187afa311974b41755b",
+    "url": "/static-business/static/js/runtime~main.1187afa3.js"
+  },
+  {
+    "revision": "2a7fa893909026fdbd32",
+    "url": "/static-business/static/js/main.2a7fa893.chunk.js"
+  },
+  {
+    "revision": "7020ce25118c011e661f",
+    "url": "/static-business/static/js/1.7020ce25.chunk.js"
+  },
+  {
+    "revision": "2a7fa893909026fdbd32",
+    "url": "/static-business/static/css/main.f99e1d21.chunk.css"
+  },
+  {
+    "revision": "7020ce25118c011e661f",
+    "url": "/static-business/static/css/1.c6ac6491.chunk.css"
+  },
+  {
+    "revision": "aa477224e0799a0a5db95792f9ab7697",
+    "url": "/static-business/index.html"
+  }
+];
